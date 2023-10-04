bb2932278692ca67c8b45d91e7f337d87768c203
tc <tczhong24@gmail.com>
2023-10-03 21:06:45 -0700

    read from local config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import json
 import os
 import sys
 
-PATH = os.path.dirname(sys.argv[0])
+USER_HOME = os.path.expanduser("~") + "/.ai_button"
 
 
 @dataclasses.dataclass
@@ -14,13 +14,15 @@ class AIConfig:
 
 
 def read_config():
-    if not os.path.exists(os.path.join(PATH, "config.json")):
+    if not os.path.exists(USER_HOME):
+        os.mkdir(USER_HOME)
+    if not os.path.exists(os.path.join(USER_HOME, "config.json")):
         return AIConfig("Key.ctrl", "`", os.environ.get("OPENAI_API_KEY") or "")
-    with open(os.path.join(PATH, "config.json"), "r", encoding="utf-8") as f:
+    with open(os.path.join(USER_HOME, "config.json"), "r", encoding="utf-8") as f:
         config = json.load(f)
     return AIConfig(**config)
 
 
 def write_config(config: AIConfig):
-    with open(os.path.join(PATH, "config.json"), "w", encoding="utf-8") as f:
+    with open(os.path.join(USER_HOME, "config.json"), "w", encoding="utf-8") as f:
         json.dump(dataclasses.asdict(config), f, indent=4)
